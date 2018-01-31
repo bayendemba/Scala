ce5f63c0637cb3f43642ffadff5d3c0cef71e400
bayendemba <bayendemba@gmail.com>
2018-01-31 19:07:09 +0000

    Counter

--- fonction.scala
+++ fonction.scala
@@ -1,3 +1,12 @@
 def carree(c:Int):Int = c*c
+  carree(2)
 
-carree(2)
+def addition(x:Double, y:Double): Double = x + y
+  addition(12+2)
+
+
+// fonction qui incremente de 1
+var x = 0
+
+def compter {x+=1;println("Compteur: " + x)}
+compter
